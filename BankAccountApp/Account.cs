50535ab5f945dd305a30735f77db17e8832e3fd3
Tanzil <tanjiluits70@gmail.com>
2015-08-29 13:07:01 -0700

    Bank account app added

---
+++ BankAccountApp/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApp
+{
+   public  class Account
+    {
+        public string name { get; set; }
+        public double blance { get;  private set; }
+        public string  account { get;  private set; }
+       public double amount { get; set; }
+
+       public Account(string Name, double Amount, string  Account): this(Name,Account)
+       {
+           amount = Amount;
+       }
+       public Account(string Name, string  Account): this(Name)
+       {
+           account = Account;
+
+       }
+
+
+       public Account(string Name): this()
+       {
+           name = Name;
+       }
+
+       public Account()
+       {
+           
+       }
+
+        public string GetDeposit( double amount)
+        {
+           blance =blance+amount;
+            return amount + " taka has been deposited.";
+        }
+
+        public string GetWithdraw( double amount)
+        {
+             blance -= amount;
+             return amount + " taka has been Withdrawn.";
+
+        }
+
+    }
+}
---
+++ BankAccountApp/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankAccountApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Account anAccount;
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            anAccount=new Account(nameTextBox.Text,accNoTextBox.Text);
+            anAccount.name = nameTextBox.Text;
+            MessageBox.Show("Name: " +anAccount.name + "Account Number: " + anAccount.account);
+            nameTextBox.Text = "";
+            accNoTextBox.Text = "";
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            anAccount.amount = Convert.ToDouble(amtTextBox.Text);
+           MessageBox.Show( anAccount.GetDeposit(anAccount.amount));
+            amtTextBox.Text = "";
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            anAccount.amount = Convert.ToDouble(amtTextBox.Text);
+           MessageBox.Show( anAccount.GetWithdraw(anAccount.amount));
+           amtTextBox.Text = "";
+
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+           string userMessage =anAccount.name + ", your account number: " + anAccount.account +
+                              " and it's balance: " + anAccount.blance;
+            MessageBox.Show(userMessage);
+        }
+    }
+}
